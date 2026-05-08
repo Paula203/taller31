e7955f615b6bd3c21efcea46469faf2c29e496a2
Paula <est.mariap.amaya@unimilitar.edu.co>
2026-05-08 12:58:24 -0500

    implemento ventanas de recorte y casos de pruebas

--- sc.js
+++ sc.js
@@ -9,4 +9,16 @@ function actualizarViewport() {
   mostrarEscena();
 }
 
+let xmin = 200;
+let ymin = 150;
+let xmax = 500;
+let ymax = 350;
+
+const lineas = [
+  {x1:250, y1:200, x2:450, y2:300},
+  {x1:100, y1:250, x2:300, y2:250},
+  {x1:250, y1:250, x2:600, y2:250},
+  {x1:100, y1:100, x2:600, y2:400},
+  {x1:50, y1:50, x2:150, y2:100}
+];
 
